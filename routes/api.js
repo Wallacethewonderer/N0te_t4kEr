06afb7858db063668d559ba9a802db67d4464a2e
Wallacethewonderer <123340192+Wallacethewonderer@users.noreply.github.com>
2023-07-18 23:31:18 -0700

    organized-ouput-format

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const router = require("express").Router();
 const address = path.join(__dirname, "../db/db.json");
 const { v4 } = require("uuid");
-const data = require(address)
-console.log(data)
+const data = require(address);
+console.log(data);
 
 router.get("/notes", (req, res) => {
 	res.sendFile(address);
@@ -17,11 +17,11 @@ router.post("/notes", (res, req) => {
 		text: res.body.text,
 		id: v4(),
 	};
-	console.log(note)
+	console.log(note);
     data.push(note)
-	console.log(data)
-    fs.writeFileSync(address, JSON.stringify(data))
-    res.json(data)
+	console.log(data);
+    fs.writeFileSync(address, JSON.stringify(data, null, 2));
+    res.json(data);
 	
 });
 
